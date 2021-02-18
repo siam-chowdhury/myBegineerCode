9c9367eda737514b5d07f40a64792271291c4d1e
siam-chowdhury <78899340+siam-chowdhury@users.noreply.github.com>
2021-02-18 19:46:44 +0600

    Update automateWifiPurchase.py

--- automateWifiPurchase.py
+++ automateWifiPurchase.py
@@ -1,20 +1,19 @@
 import random
-import requests
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-from bs4 import BeautifulSoup
 
 
 
 
 class Wifi:
 
-    url = ""
-    text = ""
+    src = ""
+    text = "Card number is invalid."
     driver = ""
     finalScratchNumber = ""
     rechargePageUrl = ""
 
+    # letters = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','0','1','2','3','4','5','6','7','8','9']
     letters = ['A','B','C','D','E','0','1','2','3','4','5','6','7','8','9']
 
 
@@ -36,45 +35,39 @@ class Wifi:
 
 
         # fillup username, password and click Login button 
-        username = self.driver.find_element_by_name("username").send_keys("pn1629")
-        password = self.driver.find_element_by_name("password").send_keys("pn1629")
+        username = self.driver.find_element_by_name("username").send_keys("pn1628")
+        password = self.driver.find_element_by_name("password").send_keys("pn1628")
         self.driver.find_element_by_class_name("login100-form-btn").click()
 
 
         # go to Recharge Page
         self.driver.find_element_by_link_text("Recharge").click()
-        # self.driver.get("http://119.148.40.171/armyris/recharge-account.php")
 
 
         # create random number and click on Recharge button
         self.driver.find_element_by_name("card_no").send_keys(self.SCRATCH_NUMBER_GENERATOR())
+        self.finalScratchNumber = ""
         self.driver.find_element_by_css_selector("input[value=Recharge]").click()
+    
 
-
-        # get current url and request to get html code
-        self.url = self.driver.current_url
-        r = requests.get(self.url)
-        htmlContent = r.content
-
-
-        # get only text from html 
-        soup = BeautifulSoup(htmlContent, "html.parser")
-        self.text = soup.get_text()
+        self.src = self.driver.page_source
         
-        # return self.text
-        print(self.text)
+
     
 
     # loop until Card number get valid
     def INVALID_CARD(self):
 
-        if "Card number is invalid." in self.text:
-
-            while True:
+        while True:
+            if self.text in self.src:
+                self.driver.find_element_by_name("card_no").clear()
                 self.driver.find_element_by_name("card_no").send_keys(self.SCRATCH_NUMBER_GENERATOR())
+                self.finalScratchNumber = ""
                 self.driver.find_element_by_css_selector("input[value=Recharge]").click()
+            
+            else:
+                break
 
-        # print(self.text)
 
     
     # Control all Function
@@ -88,4 +81,3 @@ class Wifi:
         
 m = Wifi()
 print(m.MAIN_FUNCTION())
-print(m.AUTOFILL_CREDENTIAL())
