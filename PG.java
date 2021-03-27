738f935732db5ccccb808b4877c06e031ee294da
siam-chowdhury <78899340+siam-chowdhury@users.noreply.github.com>
2021-03-27 23:13:19 +0600

    Add files via upload

---
+++ PG.java
@@ -0,0 +1,17 @@
+public class PG {
+    
+
+
+    public static void main(String[] args) {
+        
+        // call passwordlen method from Lenght Class
+        Length.passwordLen();
+    }
+
+
+
+
+
+
+
+}
