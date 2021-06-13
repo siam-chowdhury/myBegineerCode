5590d1d1d1a2ad8112723ef01ecb8c8dfa13ace8
siam-chowdhury <78899340+siam-chowdhury@users.noreply.github.com>
2021-06-14 00:12:29 +0600

    Update app.js

--- Tip Calculator/app.js
+++ Tip Calculator/app.js
@@ -1,7 +1,7 @@
 
 
 
-var bill_amount, service_percent, amount_people
+var bill_amount, service_percent, amount_people;
     
 
 var get_data = () => {    
@@ -9,26 +9,25 @@ var get_data = () => {
     service_percent = document.querySelector("#Option").value;
     amount_people = document.querySelector("#pplAmount").value;
     
-    // calculate_tip(bill_amount, service_percent, amount_people);
-
-    var service = parseInt(service_percent);
-    service /= 100;
-
-    var tip = (bill_amount*service)/amount_people;
-    var fianl_tip = parseFloat(tip).toFixed(2);
-    document.querySelector("#tip").innerHTML = fianl_tip;
-
-
-
-
+    calculate_tip(bill_amount, service_percent, amount_people);
 }
 
 
-// var calculate_tip = (bill_amount, service_percent, amount_people) => {
+var calculate_tip = (bill_amount, service_percent, amount_people) => {
 
-//     alert(service_percent)
+    var tip, fianl_tip;
+    var service = parseInt(service_percent);
+    service /= 100;
+
+    if (amount_people == 0) {
+        amount_people = 1;
+    }
+    
+    tip = (bill_amount*service)/amount_people;    
+    fianl_tip = parseFloat(tip).toFixed(2);
+    document.querySelector("#tip").innerHTML = fianl_tip;
+
+    
 
 
-// }
-
-// calculate_tip()
+}
